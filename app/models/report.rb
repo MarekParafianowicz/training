60ffc3c17280317ead3c7a786c80850d915f8348
marek parafianowicz <marek.parafianowicz@netguru.co>
2017-03-04 19:30:14 +0100

    Clean code

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,14 +1,8 @@
 class Report < ActiveRecord::Base
 
-<<<<<<< HEAD
   def publish
     sleep_time 5
     update_attribute(:published_at, Time.zone.now)
-=======
-  def save_report
-    sleep_time(10)
-    self.save
->>>>>>> Custom save method with sleep
   end
 
   def sleep_time(time)
